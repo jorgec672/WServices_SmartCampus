caaff07b1dd507845e94e405afaf01ed9a91abfe
jorgec672 <jorgec672@gmail.com>
2017-08-03 13:23:46 -0500

    Se crea archivo .properties para carga de conexion BD

--- WS_cargaAcademica/src/uniajc/edu/ws/WSCargaAcademica.java
+++ WS_cargaAcademica/src/uniajc/edu/ws/WSCargaAcademica.java
@@ -14,9 +14,6 @@ public class WSCargaAcademica {
 	private CreaJson jsonUtil = CreaJson.getJson();
 	
     /***
-    * Retorna un   Listado  de  Objetos tipo HistorialPrograma con sus respectivas Materias  y calificaciones,
-    * el retorno del objeto  se   realiza bajo  notación JSON. 
-    *  <i>(presenta  la traza de asignaturas, y estados del programa a lo largo de la carrera cursada)</i>
     * @param ID    <i>(Identificación unica del Profesor en Academusoft) </i>
     * @return  <b>String (Notación JSON)</b>
    ***/
